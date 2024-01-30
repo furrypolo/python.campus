7613bf0a8f7c5ab3497f91d97495f24617fa4d4a
briggea <geasandhea19@gmail.com>
2024-01-30 23:27:38 +0700

    midway backup

---
+++ word guessing.py
@@ -0,0 +1,27 @@
+import random
+
+name=input("What's your name? ")
+print(" ")
+print("Welcome ",name,"!")
+print(" ")
+words=['cabbage', 'book', 'bottle', 'oven', 'pencil']
+
+word=random.choice(words)
+
+print("It's a singular object")
+
+turns=10
+
+# while turns>0:
+
+# import random
+
+# cars = ["Ford", "Volvo", "BMW", "Star-gazer", "IONIQ"]
+
+# num =(random.randint(0,4))
+
+# cars.append("Mustang")
+
+# x=cars[5]
+
+# print(x)

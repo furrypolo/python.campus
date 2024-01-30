edee8d690be13ce962ba01ddeacaedb0bf21a005
briggea <geasandhea19@gmail.com>
2024-01-30 22:42:30 +0700

    initial study addition

---
+++ random number.py
@@ -0,0 +1,24 @@
+import random
+
+print("Hello! Welcome to number-guess arcade")
+print("Please input your range ")
+min=int(input("minimum value= "))
+max=int(input("maximum value= "))
+
+pick=(random.randint(min,max))
+print(pick)
+
+print("You have 10 chances!")
+for x in range(10):
+    guess=int(input("What's your guess? "))
+    if(guess==pick):
+        break
+    elif(guess>pick):
+        print("Sorry your guess is too high")
+    else:
+        print("Sorry your guess is too low")
+
+if(guess==pick):
+    print("Yes! You won!")
+else:
+    print("Sorry, you ran out of chances!")
---
+++ test.py
@@ -0,0 +1,6 @@
+print("This is file 1")
+print(50)
+alas=int(input("Input your alas integer: "))
+tinggi=int(input("Input your tinggi integer: "))
+hasil=alas*tinggi
+print("Hasilnya adalah ",hasil)
